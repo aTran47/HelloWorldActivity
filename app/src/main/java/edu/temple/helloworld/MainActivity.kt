3173e03fbbd4a748655a49e7f6f2d6415d440c94
aTran47 <tuk82621@temple.edu>
2023-09-14 15:17:20 -0400

    Added button click listener that displayed the entered text concatenated to string "Hello, "

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,17 +10,20 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
-
+    lateinit var clickMeButton: Button
+    lateinit var nameEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        clickMeButton = findViewById(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.nameEditText)
 
         
         // Respond to button click event per specifications
-
+        clickMeButton.setOnClickListener{displayTextView.text="Hello, " + nameEditText.text}
 
     }
 }
